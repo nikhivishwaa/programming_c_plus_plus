ee743995273a838a6b4a1b829e4b14f3277e000b
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2023-01-09 21:16:58 +0530

    Add files via upload

---
+++ basic concepts of c++/Control Statements/control_structure.cpp
@@ -0,0 +1,20 @@
+/*
+        they are 3 types :
+
+        1. sequence stucture
+        
+        2. selection structure
+            a. if-else statements
+                --> if statement
+                --> if-else statement
+                --> Nested if-else statement
+                --> if-else if ledder
+            b. switch case statements
+
+        3. loop structure 
+            a. for loop         // entry control
+            b. while loop       // entry control
+            c. do while loop        // exit control
+
+
+*/
